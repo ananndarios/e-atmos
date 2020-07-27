dce97c193bf13a5677ac033a011cc46f2a533224
ANANNDA RIOS DOS SANTOS MOURA <ANANNDA.MOURA@BRADESCO.COM.BR>
2020-07-26 22:53:55 -0300

    Subindo o projeto

---
+++ js/map.js
@@ -0,0 +1,13 @@
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position)
+    }, function (error) { ...optionalParams: any[]): void
+        console.log(console.error();
+
+
+} else {
+
+    alert('ops, não foi possível verificar a posição')
+
+};        
+
